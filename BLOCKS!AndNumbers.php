0fb2d3ceea23aa58255894e76c7e07238bb1073e
Justus Bayat <61127228+p47even@users.noreply.github.com>
2020-03-09 16:09:22 -0700

    Update BLOCKS!AndNumbers.php

--- BLOCKS!AndNumbers.php
+++ BLOCKS!AndNumbers.php
@@ -16,7 +16,6 @@
 			/**
 				This code makes random colored blocks fall from the sky. It was writen by me, Alex Meislich, on 3/5/20 (the day will live in infamy).
 				Feel free to edit it and play around with the JavaScript (or JS). 
-
 			*/
 			const canvas = document.querySelector('.myCanvas'); //JS witchery to get the canvas set up
 			const width = canvas.width = window.innerWidth;	//make the canvas the width and height of the entire page
@@ -29,6 +28,7 @@
 			const moveDist = gap * speed;
 			var go; //this variable will be in charge of starting/stopping the animation
 			var	blocks = new Array(); //this blocks array will hold our Block objects that are on the screen
+			var problems = new Array();
 			var counter = 0;
 			b1 = new Block(0, "red"); //make some blocks to the array just to flex
 			b2 = new Block(300, "blue");
@@ -38,24 +38,35 @@
 			blocks.push(b3);
 			start(); //start the animation
 
+			function getRandomInt(){ //justus
+				var min = Math.ceil(0);
+				var max = Math.floor(10);
+				return Math.floor(Math.random() * (max - min+1)) + min;
+			}
+
+			function generateAddition(){ //justus
+				return (getRandomInt()) + " + " + (getRandomInt()) ;
+			}
+
+
 			function start(){
 				go = setInterval(redraw, gap); //run redraw (I don't know why it doesn't have parameters)  every gap seconds
 			}
-
 			function stop(){
 				clearInterval(go); //stop the animation
 			}
-
 			function redraw(){ //this method redraws the frame
 				++counter;
 				Image.clearRect(0, 0, canvas.width, canvas.height); //clear the existing frame.
 				num = parseInt(Math.random() * 1351, 10); //give me a random int between 0 and 1350
 				if(num % 200 === 0){	//if it is evenly divisable by 200 make a new block with a random color and put it on that spot on the x axis
 					blocks.push(new Block(num, "random"));
+					problems.push(generateAddition());
 					console.log(counter);
 				}
 				for(i = 0; i < blocks.length; i++){ //loop through the blocks array and draw them all. 
 					block = blocks[i];
+					currProb = problems[i];
 					block.draw();
 					if(block.getY() > 1000){ //if we find one that isn't on the screen delete it from the front of the array. There isn't any use drawing it or storing it if we can't see it.
 						blocks.shift();
@@ -86,7 +97,6 @@
 					r = parseInt(Math.random()*256, 10);//this parseInt casts the double to an int the "10" represents radix 10. Always use radix 10.
 					g = parseInt(Math.random()*256, 10);
 					b = parseInt(Math.random()*256, 10);
-
 				}
 				this.move = function(){
 					y = y + moveDist; //move the block
@@ -94,7 +104,7 @@
 					ctx.fillRect(x+20,y+75,175,100);
 					ctx.fillStyle = 'black';
 					ctx.font = '50px serif'; // Sets font size and font type to 
-					ctx.fillText(' 2 + 2 = ',x +25,y + 150);// what to write and where to write it
+					ctx.fillText(currProb,x +25,y + 150);// what to write and where to write it
 				}
 				this.getColor = function(){
 					return 'rgb(' + r + ',' + g + ',' + b + ')'; //return the color
@@ -103,7 +113,6 @@
 					Image.fillStyle = block.getColor();//set the color we need to draw
 					Image.fillRect(x, y, width, height); //draw the block
 					this.move(); //move the block
-
 				}
 				this.getY = function(){ //return the y
 					return y;
@@ -111,6 +120,5 @@
 			}
 		</script>
 	</canvas>
-
 </body>
 </html>

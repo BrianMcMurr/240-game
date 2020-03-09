10ff6fc8651421b173ece2ef4ff066e274e0bb1d
Maxusomega <57078371+Maxusomega@users.noreply.github.com>
2020-03-09 16:26:42 -0700

    meme potential
    Upload that music

---
+++ Attempt1.php
@@ -0,0 +1,26 @@
+<html>
+
+<audio src="Sit Next to Me.m4a" autoplay="true">
+<div style="border: 1px solid black ; padding: 1px ;">
+Sorry, your browser does not support the <audio> tag used in this demo.
+</div>
+</audio>
+
+
+
+<?php
+
+echo "Hello, this is song";
+##<embed src="/Sit Next to Me.m4a" autostart="true" loop="true">
+##<embed src=“pathname/filename”>
+##<audio src="Sit Next to Me.m4a" autostart="true"></audio>
+/*
+<audio>
+  <source src="Sit Next to Me.m4a" type="audio/m4a">
+Your browser does not support the audio element.
+</audio>
+
+*/
+?>
+
+<html/>

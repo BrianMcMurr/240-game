6515db186dfad020c104a266cf5cfc36b7ad9600
Justus Bayat <61127228+p47even@users.noreply.github.com>
2020-03-10 13:16:26 -0700

    Update BLOCKS!AndNumbers.php

--- BLOCKS!AndNumbers.php
+++ BLOCKS!AndNumbers.php
@@ -34,23 +34,22 @@ Sorry, your browser does not support the <audio> tag used in this demo.
 			const moveDist = gap * speed;
 			var go; //this variable will be in charge of starting/stopping the animation
 			var	blocks = new Array(); //this blocks array will hold our Block objects that are on the screen
-			var problems = new Array();
 			var counter = 0;
-			b1 = new Block(0, "red"); //make some blocks to the array just to flex
-			b2 = new Block(300, "blue");
-			b3 = new Block(600, "green");
+			b1 = new Block(0, "red", generateAddition()); //make some blocks to the array just to flex
+			b2 = new Block(300, "blue", generateAddition());
+			b3 = new Block(600, "green", generateAddition());
 			blocks.push(b1); //add them to the array to flex harder
 			blocks.push(b2);
 			blocks.push(b3);
 			start(); //start the animation
 
-			function getRandomInt(){ //justus
+			function getRandomInt(){ 
 				var min = Math.ceil(0);
 				var max = Math.floor(10);
 				return Math.floor(Math.random() * (max - min+1)) + min;
 			}
 
-			function generateAddition(){ //justus
+			function generateAddition(){ 
 				return (getRandomInt()) + " + " + (getRandomInt()) ;
 			}
 
@@ -63,16 +62,15 @@ Sorry, your browser does not support the <audio> tag used in this demo.
 			}
 			function redraw(){ //this method redraws the frame
 				++counter;
+				var problem = generateAddition();
 				Image.clearRect(0, 0, canvas.width, canvas.height); //clear the existing frame.
 				num = parseInt(Math.random() * 1351, 10); //give me a random int between 0 and 1350
 				if(num % 200 === 0){	//if it is evenly divisable by 200 make a new block with a random color and put it on that spot on the x axis
-					blocks.push(new Block(num, "random"));
-					problems.push(generateAddition());
+					blocks.push(new Block(num, "random", problem));
 					console.log(counter);
 				}
 				for(i = 0; i < blocks.length; i++){ //loop through the blocks array and draw them all. 
 					block = blocks[i];
-					currProb = problems[i];
 					block.draw();
 					if(block.getY() > 1000){ //if we find one that isn't on the screen delete it from the front of the array. There isn't any use drawing it or storing it if we can't see it.
 						blocks.shift();
@@ -80,7 +78,8 @@ Sorry, your browser does not support the <audio> tag used in this demo.
 					}
 				}
 			}
-			function Block(ex, color){ //this is the Blocks class. We will use it for all of our block creating needs. 
+			function Block(ex, color, prob){ //this is the Blocks class. We will use it for all of our block creating needs. 
+				var problem = prob;
 				var width = 200; //blocks are this big.
 				var height = 200;
 				var x = ex; //set the position. (blocks start at the top of the screen by default)
@@ -110,7 +109,7 @@ Sorry, your browser does not support the <audio> tag used in this demo.
 					ctx.fillRect(x+20,y+75,175,100);
 					ctx.fillStyle = 'black';
 					ctx.font = '50px serif'; // Sets font size and font type to 
-					ctx.fillText(currProb,x +25,y + 150);// what to write and where to write it
+					ctx.fillText(problem,x +25,y + 150);// what to write and where to write it
 				}
 				this.getColor = function(){
 					return 'rgb(' + r + ',' + g + ',' + b + ')'; //return the color
